ce4b3b6addc669e8931dd7c4f5c82941f04b04b8
aweroras <hannahhbusto@gmail.com>
2024-03-09 14:16:16 +0800

    changed front page from welcome to index

--- routes/web.php
+++ routes/web.php
@@ -8,41 +8,20 @@ use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\SupplierController;
 use App\Http\Controllers\Auth\AccountController;
 
+// Public routes accessible by all users
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
+// Login and registration routes
+Route::post('/login', [AccountController::class, 'login'])->name('login');
+Route::get('/loginform', [AccountController::class, 'loginform'])->name('login.form');
+Route::get('/signup', [AccountController::class, 'signupform'])->name('signup.form');
+Route::post('/register/customer', [AccountController::class, 'register'])->name('register.customer');
+Route::get('/login', [AccountController::class, 'loginform'])->name('login.form');
 
-
-//admin dashboard
-Route::get('admin/products', [ProductController::class, 'index'])->name('admin.products.index');
-Route::get('admin/products/create', [ProductController::class, 'create'])->name('admin.products.create');
-Route::post('admin/products', [ProductController::class, 'store'])->name('admin.products.store');
-Route::get('/admin/products/{product}/edit', [ProductController::class, 'edit'])->name('admin.products.edit');
-Route::put('/admin/products/{product}/update', [ProductController::class, 'update'])->name('admin.products.update');
-Route::get('admin/products/{product}/delete', [ProductController::class, 'destroy'])->name('admin.products.delete');
-
-//Supplier
-Route::get('admin/supplier/index', [SupplierController::class, 'index'])->name('supplier.index');
-Route::get('admin/supplier/create', [SupplierController::class, 'create'])->name('supplier.create');
-Route::post('admin/supplier/store', [SupplierController::class, 'store'])->name('supplier.store');
-Route::get('admin/supplier/update/{id}', [SupplierController::class, 'update'])->name('supplier.update');
-Route::put('/admin/supplier/edit/{id}', [SupplierController::class, 'edit'])->name('supplier.edit');
-Route::get('/admin/supplier/delete/{id}', [SupplierController::class, 'delete'])->name('supplier.delete');
-
-//user dashboard
-// Route::get('user', [CustomerController::class, 'index'])->name('user.index');
-// Route::get('user/cart', [CustomerController::class, 'cart'])->name('user.cart');
-// Route::get('/user/add-to-cart/{id}', [CustomerController::class, 'addToCart'])->name('user.addToCart');
-// Route::delete('/user/remove-from-cart/{key}', [CustomerController::class, 'removeFromCart'])->name('user.removeFromCart');
-// Route::put('user/update-quantity/{key}', [CustomerController::class, 'updateQuantity'])->name('user.updateQuantity');
-// Route::get('/user/product/{id}', [CustomerController::class, 'viewProduct'])->name('user.product');
-// Route::post('/user/add-to-cart/{id}', [CustomerController::class, 'addToCart'])->name('user.addToCart');
-// Route::get('user/search-query', [CustomerController::class, 'search'])->name('user.search-query');
-
-
-//Customer dashboard (has to be logged in to access)
-Route::middleware('auth')->group(function () {
+// Customer dashboard routes 
+Route::middleware(['auth'])->group(function () {
     Route::get('customer', [CustomerController::class, 'index'])->name('customer.index');
     Route::get('customer/shop', [CustomerController::class, 'shop'])->name('customer.shop');
     Route::get('/customer/details/{id}', [CustomerController::class, 'details'])->name('customer.details');
@@ -54,18 +33,21 @@ Route::middleware('auth')->group(function () {
     Route::delete('/customer/remove-from-cart/{key}', [CustomerController::class, 'removeFromCart'])->name('customer.removeFromCart');
 });
 
-//login and regis
-Route::post('/login', [AccountController::class, 'login'])->name('login');
-Route::get('/loginform', [AccountController::class, 'loginform'])->name('login.form');
-Route::get('/signup', [AccountController::class, 'signupform'])->name('signup.form');
-Route::post('/register/customer', [AccountController::class, 'register'])->name('register.customer');
-Route::get('/login', [AccountController::class, 'loginform'])->name('login.form');
 
 
 
-//login routes
-Route::post('/login', [AccountController::class, 'login'])->name('login');
-Route::get('/loginform', [AccountController::class, 'loginform'])->name('login.form');
-Route::get('/signup', [AccountController::class, 'signupform'])->name('signup.form');
-Route::post('/register/customer', [AccountController::class, 'register'])->name('register.customer');
-Route::get('/login', [AccountController::class, 'loginform'])->name('login.form');
+// Admin dashboard routes
+Route::get('admin/products', [ProductController::class, 'index'])->name('admin.products.index');
+Route::get('admin/products/create', [ProductController::class, 'create'])->name('admin.products.create');
+Route::post('admin/products', [ProductController::class, 'store'])->name('admin.products.store');
+Route::get('/admin/products/{product}/edit', [ProductController::class, 'edit'])->name('admin.products.edit');
+Route::put('/admin/products/{product}/update', [ProductController::class, 'update'])->name('admin.products.update');
+Route::get('admin/products/{product}/delete', [ProductController::class, 'destroy'])->name('admin.products.delete');
+
+// Supplier routes
+Route::get('admin/supplier/index', [SupplierController::class, 'index'])->name('supplier.index');
+Route::get('admin/supplier/create', [SupplierController::class, 'create'])->name('supplier.create');
+Route::post('admin/supplier/store', [SupplierController::class, 'store'])->name('supplier.store');
+Route::get('admin/supplier/update/{id}', [SupplierController::class, 'update'])->name('supplier.update');
+Route::put('/admin/supplier/edit/{id}', [SupplierController::class, 'edit'])->name('supplier.edit');
+Route::get('/admin/supplier/delete/{id}', [SupplierController::class, 'delete'])->name('supplier.delete');
