8d68eb7ea9d41977cb608ffbd3db5d671cabf789
jojotrance <139723099+jojotrance@users.noreply.github.com>
2024-03-09 23:23:01 +0800

    Update sidebar.blade.php

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -43,7 +43,7 @@
   </li>
 
   <li class="nav-item">
-    <a class="nav-link" href="user">
+    <a class="nav-link" href="{{ route('reviews.index') }}">
       <i class="fas fa-fw fa-tachometer-alt"></i>
       <span>Reviews</span></a>
   </li>
