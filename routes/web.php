1e9a30cd90f77c41d030337c131c3e3c38350e46
jojotrance <139723099+jojotrance@users.noreply.github.com>
2024-03-09 23:14:22 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\UserProductController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\Auth\AccountController;
 use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\ReviewController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -29,6 +30,15 @@ Route::post('admin/supplier/store', [SupplierController::class, 'store'])->name(
 Route::get('admin/supplier/update/{id}', [SupplierController::class, 'update'])->name('supplier.update');
 Route::put('/admin/supplier/edit/{id}', [SupplierController::class, 'edit'])->name('supplier.edit');
 Route::get('/admin/supplier/delete/{id}', [SupplierController::class, 'delete'])->name('supplier.delete');
+
+//Reviews
+Route::get('/admin/reviews/index', [ReviewController::class, 'index'])->name('reviews.index');
+Route::get('admin/reviews/create', [ReviewController::class, 'create'])->name('reviews.create');
+Route::post('/admin/reviews/store', [ReviewController::class, 'store'])->name('reviews.store');
+
+
+// Route::get('/{review}', 'ReviewController@show')->name('reviews.show'); -- specific review to di ko pa natatry
+
 
 //user dashboard
 // Route::get('user', [CustomerController::class, 'index'])->name('user.index');
