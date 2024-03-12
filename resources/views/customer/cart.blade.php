6efa473df57819e4cf2482537bb80c691330304c
jojotrance <139723099+jojotrance@users.noreply.github.com>
2024-03-12 19:04:26 +0800

    checkout button

--- resources/views/customer/cart.blade.php
+++ resources/views/customer/cart.blade.php
@@ -111,7 +111,7 @@
                         </table>
                         <div class="checkout_btn_inner float-right">
                             <a class="btn_1" href="{{ route('customer.shop') }}">Continue Shopping</a>
-                            <a class="btn_1 checkout_btn_1" href="#">Proceed to checkout</a>
+                            <a class="btn_1 checkout_btn_1" href="{{ route('customer.checkout') }}">Proceed to checkout</a>
                         </div>
                     </div>
                 </div>
