4d4546dd873262670943c4c7bd49c58f38635918
JoshNotheGreat <Joshziemenntan998@gmail.com>
2024-03-17 21:54:22 +0800

    Update Customer.php

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -5,11 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\belongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 
 class Customer extends Model
 {
     use HasFactory;
-    protected $table = 'customer_info';
     protected $fillable = [
         'user_id',
         'name',
@@ -22,4 +23,10 @@ class Customer extends Model
     {
         return $this->belongsTo(User::class, 'user_id');
     }
+
+    public function order()
+    {
+        return $this->hasMany(Orderinfo::class);
+    }
+
 }
