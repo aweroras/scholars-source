9bc797a7be4d7c8f0b9b8c4821a26ba174b4353e
JoshNotheGreat <Joshziemenntan998@gmail.com>
2024-03-17 21:56:48 +0800

    Update 2024_03_02_030448_create_order_product_table.php

--- database/migrations/2024_03_02_030448_create_order_product_table.php
+++ database/migrations/2024_03_02_030448_create_order_product_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamps();
 
             //Fk
-            $table->foreign('order_id')->references('id')->on('order_info')->onDelete('cascade');
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
             $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
         });
     }
