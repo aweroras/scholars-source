5c3c68d433d4bb61273a34a6dc6f68d5e20c26cf
JoshNotheGreat <Joshziemenntan998@gmail.com>
2024-03-28 20:48:01 +0800

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->string('roles');
+            $table->string('status');
             $table->timestamps();
         });
     }
