b47a53323f311f5161ea8d3406c436987ddb4b0f
JoshNotheGreat <Joshziemenntan998@gmail.com>
2024-04-02 00:59:56 +0800

    Update index.blade.php

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -2,24 +2,27 @@
 
 @section('content')
     <h1>Orders</h1>
-
-    <table>
-        <thead>
+    <table id="supplierTable" class="table table-hover">
+        <thead class="table-primary">
             <tr>
                 <th>Order ID</th>
                 <th>Status</th>
-                <th>Actions</th>
+                <th>Payment Method</th>
+                <th>Courier</th>
+                <th>Action</th>
+                <th>Date Ordered</th>
             </tr>
         </thead>
         <tbody>
             @foreach ($orders as $order)
-                <tr>
-                    <td>{{ $order->id }}</td>
-                    <td>{{ $order->status }}</td>
-                    <td>
-                        <a href="{{ route('admin.orders.update', $order) }}">Update Status</a>
-                    </td>
-                </tr>
+            <tr>
+                <td class="align-middle">{{ $order->id }}</td>                
+                <td class="align-middle">{{ $order->status }}</td>
+                <td class="align-middle">{{ $order->payment_method}}</td>
+                <td class="align-middle">{{ $order->courier}}</td>
+                <td class="align-middle"><a href="{{ route('admin.orders.update', $order) }}">Change Status</a></td>
+                <td class="align-middle">{{ $order->created_at }}</td>
+            </tr>
             @endforeach
         </tbody>
     </table>
