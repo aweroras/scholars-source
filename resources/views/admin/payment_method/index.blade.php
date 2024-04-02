a79e979d31566cb301382d49165403e487a3e9fc
aweroras <hannahhbusto@gmail.com>
2024-04-02 13:04:37 +0800

    soft delete restore

--- resources/views/admin/payment_method/index.blade.php
+++ resources/views/admin/payment_method/index.blade.php
@@ -2,8 +2,17 @@
 
 @section('content')
 <div class="container">
-    <h1>Payment Methods</h1>
-    <a href="{{ route('admin.payment_method.create') }}" class="btn btn-primary">Add New Payment Method</a>
+    <div class="d-flex align-items-center justify-content-between mb-3">
+        <h1>Payment Methods</h1>
+        <div class="d-flex align-items-center">
+            <a href="{{ route('admin.payment_method.create') }}" class="btn btn-primary mr-2">Add New Payment Method</a>
+            <form action="{{ route('admin.payment_method.restoreAll') }}" method="POST">
+                @csrf
+                <button type="submit" onclick="return confirm('Are you sure you want to restore all soft deleted payment methods?')" class="btn btn-success mr-2">Restore All</button>
+            </form>
+            
+        </div>
+    </div>
     <table id="paymentMethodTable" class="table table-hover">
         <thead class="table-primary">
             <tr>
@@ -23,12 +32,28 @@
                 </td>                
                 <td class="align-middle">{{$paymentMethod->payment_name}}</td>
                 <td class="align-middle"><a href="{{route('admin.payment_method.update', $paymentMethod->id)}}">Update</a></td>
-                <td class="align-middle"><a href="">Delete</a></td>
-               
+                <td class="align-middle">
+                    <form action="{{ route('admin.payment_method.delete', $paymentMethod->id) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" onclick="return confirm('Are you sure you want to delete this payment method?')" class="btn btn-danger">Delete</button>
+                    </form>
                 </td>
             </tr>
             @endforeach
         </tbody>
     </table>
 </div>
+
+<!-- Include DataTables CSS and JavaScript -->
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap4.min.css">
+<script type="text/javascript" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap4.min.js"></script>
+
+<!-- Initialize DataTable -->
+<script>
+    $(document).ready(function() {
+        $('#paymentMethodTable').DataTable();
+    });
+</script>
 @endsection
