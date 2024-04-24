466374608b060ce70a4c6c1fca51e6c6b0aa8cd2
JoshNotheGreat <Joshziemenntan998@gmail.com>
2024-04-24 20:21:52 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Middleware\RoleMiddleware;
 use App\Mail\Verification;
 use Illuminate\Support\Facades\Mail;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
 
 
 // Public routes accessible by all users
@@ -147,7 +150,20 @@ Route::get('/product/{id}', [HomeController::class, 'productDetails'])->name('pr
 Route::get('/about', [HomeController::class, 'about'])->name('about');
 Route::get('/search', [HomeController::class, 'search'])->name('search');
 
+Route::get('/createadmin', function () {
 
+    $admin = new User();
+    $admin->email = 'admin@gmail.com';
+    $admin->password = Hash::make('123123123');
+    $admin->email_verified_at = now();
+    $admin->status = 'Verified';
+    $admin->roles = 'admin';
+    $admin->created_at = now();
+    $admin->updated_at = now();
+    $admin->save();
+
+    return view('accounts.login');
+});
 
 
 
